25132172d327266c60a28da8f8d959bd04f19d2d
Abdhilahi R. Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2024-05-19 22:42:56 -0600

    Add exponents

---
+++ lib/src/logic/Radius.swift
@@ -0,0 +1,7 @@
+// Radius of Objects
+class Radius {
+    // Radius of a Circle
+    func circle(diameter: Double) -> Double {
+        return diameter / 2
+    }
+}
---
+++ web/lib/src/logic/radius.dart
@@ -0,0 +1,7 @@
+// Radius of Objects
+class Radius {
+  // Radius of a Circle
+  double circle(double diameter) {
+    return diameter / 2;
+  }
+}
