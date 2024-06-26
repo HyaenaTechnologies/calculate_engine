b2adc09d663f27c61ec9aebd40206f421c948342
Abdhilahi R. Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2024-06-25 19:58:15 -0600

    TODO: Finish Conversions

--- lib/src/logic/math/conversion/centimeter_conversion.zig
+++ lib/src/logic/math/conversion/centimeter_conversion.zig
@@ -1,14 +1,56 @@
 // Centimeter Conversion
 const CentimeterConversion = struct {
-    // Centimeters to Kilometers
-    pub fn centimeter_to_kilometer(kilometers: f64) f64 {
-        var centimeter: f64 = kilometers * 100000;
-        return centimeter;
+    // Decameters to Centimeters
+    pub fn decameters_to_centimeters(decameters: f64) f64 {
+        var centimeters: f64 = decameters * 1000;
+        return centimeters;
     }
 
-    // Centimeters to Meters
-    pub fn centimeter_to_meter(meter: f64) f64 {
-        var centimeter: f64 = meter * 100;
-        return centimeter;
+    // Decimeters to Centimeters
+    pub fn decimeters_to_centimeters(decimeters: f64) f64 {
+        var centimeters: f64 = decimeters * 10;
+        return centimeters;
+    }
+
+    // Hectometers to Centimeters
+    pub fn hectometers_to_centimeters(hectometers: f64) f64 {
+        var centimeters: f64 = hectometers * 10000;
+        return centimeters;
+    }
+
+    // Kilometers to Centimeters
+    pub fn kilometers_to_centimeters(kilometers: f64) f64 {
+        var centimeters: f64 = kilometers * 100000;
+        return centimeters;
+    }
+
+    // Picometers to Centimeters
+    pub fn picometers_to_centimeters(picometers: f64) f64 {
+        var centimeters: f64 = picometers * -10000000000;
+        return centimeters;
+    }
+
+    // Meters to Centimeters
+    pub fn meters_to_centimeters(meters: f64) f64 {
+        var centimeters: f64 = meters * 100;
+        return centimeters;
+    }
+
+    // Micrometers to Centimeters
+    pub fn micrometers_to_centimeters(micrometers: f64) f64 {
+        var centimeters: f64 = micrometers * -10000;
+        return centimeters;
+    }
+
+    // Millimeters to Centimeters
+    pub fn millimeters_to_centimeters(millimeters: f64) f64 {
+        var centimeters: f64 = millimeters * 0.1;
+        return centimeters;
+    }
+
+    // Nanometers to Centimeters
+    pub fn nanometers_to_centimeters(nanometers: f64) f64 {
+        var centimeters: f64 = nanometers * -10000000;
+        return centimeters;
     }
 };
--- web/lib/src/logic/math/conversion/centimeter_conversion.dart
+++ web/lib/src/logic/math/conversion/centimeter_conversion.dart
@@ -1,14 +1,56 @@
 // Centimeter Conversion
 class CentimeterConversion {
-  // Centimeters to Kilometers
-  double centimeter_to_kilometer(double kilometers) {
-    double centimeter = kilometers * 100000;
-    return centimeter;
+  // Decameters to Centimeters
+  double decameters_to_centimeters(double decameters) {
+    double centimeters = decameters * 1000;
+    return centimeters;
   }
 
-  // Centimeters to Meters
-  double centimeter_to_meter(double meter) {
-    double centimeter = meter * 100;
-    return centimeter;
+  // Decimeters to Centimeters
+  double decimeters_to_centimeters(double decimeters) {
+    double centimeters = decimeters * 10;
+    return centimeters;
+  }
+
+  // Hectometers to Centimeters
+  double hectometers_to_centimeters(double hectometers) {
+    double centimeters = hectometers * 10000;
+    return centimeters;
+  }
+
+  // Kilometers to Centimeters
+  double kilometers_to_centimeters(double kilometers) {
+    double centimeters = kilometers * 100000;
+    return centimeters;
+  }
+
+  // Picometers to Centimeters
+  double picometers_to_centimeters(double picometers) {
+    double centimeters = picometers * -10000000000;
+    return centimeters;
+  }
+
+  // Meters to Centimeters
+  double meters_to_centimeters(double meters) {
+    double centimeters = meters * 100;
+    return centimeters;
+  }
+
+  // Micrometers to Centimeters
+  double micrometers_to_centimeters(double micrometers) {
+    double centimeters = micrometers * -10000;
+    return centimeters;
+  }
+
+  // Millimeters to Centimeters
+  double millimeters_to_centimeters(double millimeters) {
+    double centimeters = millimeters * 0.1;
+    return centimeters;
+  }
+
+  // Nanometers to Centimeters
+  double nanometers_to_centimeters(double nanometers) {
+    double centimeters = nanometers * -10000000;
+    return centimeters;
   }
 }
