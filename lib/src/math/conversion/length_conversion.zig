3030d666ad4fc6f49e5d225468bad28d8e198c31
Abdhilahi R. Wabwire <124460109+AbdhilahiRWabwire@users.noreply.github.com>
2024-07-06 18:48:33 -0600

    Update Conversion Algorithm

---
+++ lib/src/math/conversion/length_conversion.zig
@@ -0,0 +1,21 @@
+// Length Units
+const lengthUnits = [18]u8{
+  "Astronomical Unit",
+  "Centimeter",
+  "Decameter",
+  "Decimeter",
+  "Feet",
+  "Hectometer",
+  "Inch",
+  "Kilometer",
+  "Light Year",
+  "Meter",
+  "Micrometer",
+  "Mile",
+  "Millimeter",
+  "Nanometer",
+  "Nautical Mile",
+  "Parsec",
+  "Picometer",
+  "Yard"
+};
